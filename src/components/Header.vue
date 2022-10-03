d851cf414e19c61c4a00727c39f4484295061307
Jhonatan1221 <108557046+Jhonatan1221@users.noreply.github.com>
2022-10-03 16:02:23 -0500

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-12 col-md-3">
+        <h1>Formulario</h1>
+      </div>
+      <div class="col-12 col-md-6"></div>
+      <img :src="url" :alt="alt" width="100">
+    </div>
+  </div>
+
+  <hr />
+
+</template>
+
+<script>
+export default {
+  data: () => ({
+    url: "https://cdn.svgporn.com/logos/vue.svg",
+    alt: "vue.js",
+  }),
+};
+</script>
+
+  
